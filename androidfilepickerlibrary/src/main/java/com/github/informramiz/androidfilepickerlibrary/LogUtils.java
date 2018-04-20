3e07f722fa1d568d670638a65da5407e29ade657
Ramiz Raja <informramiz@gmail.com>
2018-04-20 08:52:24 +0500

    feat: Add a utility class

---
+++ androidfilepickerlibrary/src/main/java/com/github/informramiz/androidfilepickerlibrary/LogUtils.java
@@ -0,0 +1,39 @@
+package com.github.informramiz.androidfilepickerlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by ramiz on 20/04/2018
+ */
+public class LogUtils {
+
+    public static void v(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            Log.v(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void w(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            Log.e(tag, msg);
+        }
+    }
+}
