8b5e39b98ba17947e0130f17f90447e890db2936
Ramiz Raja <informramiz@gmail.com>
2019-03-18 15:47:18 +0500

    refactor: Upgrade min sdk to 21

--- androidfilepickerlibrary/src/main/java/com/github/informramiz/androidfilepickerlibrary/AttachmentPickerActivity.java
+++ androidfilepickerlibrary/src/main/java/com/github/informramiz/androidfilepickerlibrary/AttachmentPickerActivity.java
@@ -2,8 +2,6 @@ package com.github.informramiz.androidfilepickerlibrary;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,18 +10,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.github.informramiz.androidfilepickerlibrary.utils.CommonUtils;
 import com.github.informramiz.androidfilepickerlibrary.utils.FileUtils;
 import com.github.informramiz.androidfilepickerlibrary.utils.LogUtils;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import droidninja.filepicker.FilePickerBuilder;
-import droidninja.filepicker.FilePickerConst;
 
 /**
  * Created by ramiz on 20/04/2018
@@ -63,8 +55,6 @@ public class AttachmentPickerActivity extends AppCompatActivity {
     public static final int REQ_CODE_CAPTURE_IMAGE = 2;
     public static final int REQ_CODE_CAPTURE_VIDEO = 3;
     public static final int REQ_CODE_SELECT_ATTACHMENT = 4;
-    public static final int REQ_CODE_SELECT_ATTACHMENT_FOR_API_BELOW_21 = 5;
-    public static final int REQ_CODE_SELECT_ATTACHMENT_BY_LIBRARY = 6;
 
     private FileInfo captureFromCameraAttachment;
 
@@ -75,16 +65,16 @@ public class AttachmentPickerActivity extends AppCompatActivity {
 
         switch (attachmentType) {
             case ATTACHMENT_TYPE_GALLERY:
-                handleGalleryAttachment();
+                showFileChooser("*/*", GALLERY_ATTACHMENT_MIME_TYPES);
                 break;
             case ATTACHMENT_TYPE_DOC:
-                handleDocAttachment();
+                showFileChooser(MAIN_MIME_TYPE_DOC_ATTACHMENT, DOC_ATTACHMENT_MIME_TYPES);
                 break;
             case ATTACHMENT_TYPE_CAPTURE_FROM_CAMERA:
                 handleCameraAttachment();
                 break;
             case ATTACHMENT_TYPE_PICK_AUDIO:
-                handleAudioAttachment();
+                showFileChooser(MAIN_MIME_TYPE_AUDIO_ATTACHMENT, AUDIO_ATTACHMENT_MIME_TYPES);
                 break;
         }
     }
@@ -138,14 +128,6 @@ public class AttachmentPickerActivity extends AppCompatActivity {
         Uri contentUri = FilePickerCustomFileProvider.getUriForFile(this, photoFile);
 
         takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        if (!isApiLollipopOrAbove()) {
-            //there are some issues on Pre-Lollipop devices with granting uri permissions
-            //so we have to use following method
-            List<ResolveInfo> resolveInfoList = getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
-            for (ResolveInfo resolveInfo : resolveInfoList) {
-                grantUriPermission(resolveInfo.activityInfo.packageName, contentUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            }
-        }
         takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
                 contentUri);
         this.startActivityForResult(takePictureIntent, REQ_CODE_CAPTURE_IMAGE);
@@ -153,55 +135,6 @@ public class AttachmentPickerActivity extends AppCompatActivity {
         FileInfo fileInfo = FilePicker.convertFileToAttachment(photoFile);
         fileInfo.setUri(contentUri);
         return fileInfo;
-    }
-
-    private void handleAudioAttachment() {
-        if (isApiLollipopOrAbove()) {
-            showFileChooser(MAIN_MIME_TYPE_AUDIO_ATTACHMENT, AUDIO_ATTACHMENT_MIME_TYPES);
-        } else {
-//            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
-            if (intent.resolveActivity(getPackageManager()) == null) {
-                Toast.makeText(this, R.string.error_msg_please_install_file_manager,
-                        Toast.LENGTH_LONG).show();
-                return;
-            }
-            startActivityForResult(intent, REQ_CODE_SELECT_ATTACHMENT_FOR_API_BELOW_21);
-        }
-    }
-
-    private void handleGalleryAttachment() {
-        if (isApiLollipopOrAbove()) {
-            showFileChooser("*/*", GALLERY_ATTACHMENT_MIME_TYPES);
-        } else {
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType(MAIN_MIME_TYPE_GALLERY_ATTACHMENT);
-            if (intent.resolveActivity(getPackageManager()) == null) {
-                Toast.makeText(this, R.string.error_msg_please_install_file_manager,
-                        Toast.LENGTH_LONG).show();
-                return;
-            }
-            startActivityForResult(intent, REQ_CODE_SELECT_ATTACHMENT_FOR_API_BELOW_21);
-        }
-    }
-
-    private void handleDocAttachment() {
-        if (isApiLollipopOrAbove()) {
-            showFileChooser(MAIN_MIME_TYPE_DOC_ATTACHMENT, DOC_ATTACHMENT_MIME_TYPES);
-        } else {
-//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//            intent.setType("file/*");
-//            startActivityForResult(intent, REQ_CODE_SELECT_ATTACHMENT_FOR_API_BELOW_21);
-            FilePickerBuilder.getInstance()
-                    .setMaxCount(1)
-                    .enableCameraSupport(false)
-                    .setActivityTheme(R.style.LibAppTheme)
-                    .pickFile(this, REQ_CODE_SELECT_ATTACHMENT_BY_LIBRARY);
-        }
-    }
-
-    private boolean isApiLollipopOrAbove() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 
     @Override
@@ -223,11 +156,8 @@ public class AttachmentPickerActivity extends AppCompatActivity {
         } else if (requestCode == REQ_CODE_CAPTURE_VIDEO) {
             attachment = FilePicker.extractAttachInfoFromUri(getApplicationContext(), data.getData());
         }
-        else if (requestCode == REQ_CODE_SELECT_ATTACHMENT
-                || requestCode == REQ_CODE_SELECT_ATTACHMENT_FOR_API_BELOW_21) {
+        else if (requestCode == REQ_CODE_SELECT_ATTACHMENT) {
             attachment = extractFileInfo(getApplicationContext(), data);
-        } else if (requestCode == REQ_CODE_SELECT_ATTACHMENT_BY_LIBRARY) {
-            attachment = onFilePickerLibraryActivityResult(data);
         }
 
         dataIntent.putExtra(DATA_ATTACHMENT, attachment);
@@ -238,9 +168,7 @@ public class AttachmentPickerActivity extends AppCompatActivity {
     private FileInfo extractFileInfo(Context context,
                                      Intent data) {
         Uri uri = data.getData();
-        if (isApiLollipopOrAbove()) {
-            takePersistablePermissions(context, data, uri);
-        }
+        takePersistablePermissions(context, data, uri);
         LogUtils.i(TAG, "Uri received from File picker: " + uri.toString());
         LogUtils.i(TAG, "Path received from File picker: " + uri.getPath());
 
@@ -248,13 +176,6 @@ public class AttachmentPickerActivity extends AppCompatActivity {
         LogUtils.i(TAG, fileInfo.toString());
 
         return fileInfo;
-    }
-
-    private FileInfo onFilePickerLibraryActivityResult(Intent data) {
-        ArrayList<String> paths = data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_DOCS);
-        //we only allow 1 file selection at the same time
-        String path = paths.get(0);
-        return FilePicker.convertFileToAttachment(path);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
